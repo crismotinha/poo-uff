5ad1ee08aa207fba2a3dffbaca36102c884849bc
Cris Motinha <cmotinha@id.uff.br>
2017-09-01 21:25:53 -0300

    (aula-2) Adiciona exercicio de calcular os divisores de um inteiro

--- POO/src/poo/HelloWorld.java
+++ POO/src/poo/HelloWorld.java
@@ -13,26 +13,26 @@ import java.util.Scanner;
  */
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        Scanner keyword = new Scanner(System.in);
-        System.out.println("Digite um valor:");
-        int cents = keyword.nextInt();
-        int one = cents/100;
-        cents = cents - (one*100);
-        int fifty = (cents/50);
-        cents = cents - (fifty*50);
-        int quarter = (cents/25);
-        cents = cents - (quarter*25);
-        int ten = (cents/10);
-        cents = cents - (ten*10);
-        int five = (cents/5);
-        cents = cents - (five*5);
-        int onecent = cents;        
-        
-        System.out.println("Moedas: \n1 real: " + one + "\n50: " + fifty
-                + "\n25: " + quarter + "\n10: " + ten + "\n5: " + five
-                + "\n1: " + onecent);
-        
-    }
+//    public static void main(String[] args) {
+//        Scanner keyword = new Scanner(System.in);
+//        System.out.println("Digite um valor:");
+//        int cents = keyword.nextInt();
+//        int one = cents/100;
+//        cents = cents - (one*100);
+//        int fifty = (cents/50);
+//        cents = cents - (fifty*50);
+//        int quarter = (cents/25);
+//        cents = cents - (quarter*25);
+//        int ten = (cents/10);
+//        cents = cents - (ten*10);
+//        int five = (cents/5);
+//        cents = cents - (five*5);
+//        int onecent = cents;        
+//        
+//        System.out.println("Moedas: \n1 real: " + one + "\n50: " + fifty
+//                + "\n25: " + quarter + "\n10: " + ten + "\n5: " + five
+//                + "\n1: " + onecent);
+//        
+//    };
     
 }
